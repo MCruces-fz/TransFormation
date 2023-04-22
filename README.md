eaa67c4f38e49aeecf022f89316c7793d4b3adc8
MCruces-fz <mcsquared.fz@gmail.com>
2023-04-22 13:10:14 +0200

    Fix walls problems of Doom view and add screen capture to README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TransFormation
+
+This project is an adaptation of the video *Creating a Doom-style 3D engine in C* of the YouTube channel **Bisqwit** ported to C#.
+
+Example in Windows Forms:
+
+[Screen Capture](./ScreenCapture.png)
--- TransFormation/Doom.cs
+++ TransFormation/Doom.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,8 @@ namespace TransFormation
         public PointF[] fPoints = new PointF[] { new PointF(1f, 1f), new PointF(1f, 1f), new PointF(1f, 1f), new PointF(1f, 1f)};
         Point origin;
         PictureBox picBox;
+
+        float hfov = 50f, vfov = 1000f; // Horizontal and Vertical Field of View
 
         public Doom(PictureBox picBox, Maze maze)
         {
@@ -37,40 +40,44 @@ namespace TransFormation
                 float tX2 = wall.point2.X - origin.X;
                 float tY2 = wall.point2.Y - origin.Y;
 
-                if (tY1 > 0 || tY2 > 0)
+                if (tY1 >= 0 && tY2 >= 0) continue;// Wall totally behind the player -> Don't draw it
+
+                if (tY1 >= 0 || tY2 >= 0) // Wall partially behind the player
                 {
-                    PointF iSx1 = intersect(tX1, tY1, tX2, tY2, -0.0001f, 0.0001f, 0f, picBox.Size.Height / 2);
-                    PointF iSx2 = intersect(tX1, tY1, tX2, tY2,  0.0001f, 0.0001f,  picBox.Size.Width, picBox.Size.Height / 2);
-                    if (tY1 <=0)
+                    float neary = 1e-4f, fary = 5, nearside = 1e-4f, farside = 20f; // Fix these numbers
+                        
+                    PointF iSect1 = intersect(tX1, tY1, tX2, tY2, -nearside, neary, -farside, fary); // Intersection left
+                    PointF iSect2 = intersect(tX1, tY1, tX2, tY2,  nearside, neary,  farside, fary); // Intersection rigth
+                    if (tY1 >= 0) // Left side behind
                     {
-                        if (iSx1.Y > 0)
+                        if (iSect1.Y < 0) 
                         {
-                            tX1 = -iSx1.X;
-                            tY1 = iSx1.Y;
+                            tX1 = iSect1.X;
+                            tY1 = iSect1.Y;
                         } else
                         {
-                            tX1 = -iSx2.X;
-                            tY1 = iSx2.Y;
+                            tX1 = iSect2.X; // tY2 < 0 por la derecha
+                            tY1 = iSect2.Y;
                         }
                     }
-                    if (tY2 <=0)
+                    if (tY2 >= 0)
                     {
-                        if (iSx1.Y > 0)
+                        if (iSect1.Y < 0)
                         {
-                            tX2 = -iSx1.X;
-                            tY2 = iSx1.Y;
+                            tX2 = iSect1.X;
+                            tY2 = iSect1.Y;
                         } else
                         {
-                            tX2 = -iSx2.X;
-                            tY2 = iSx2.Y;
+                            tX2 = iSect2.X; // tY1 < 0 por la izquierda
+                            tY2 = iSect2.Y;
                         }
                     }
                 }
 
-                this.fPoints[0] = new PointF(-50 * tX1 / tY1 + origin.X, -700f / tY1 + origin.Y); // (X1, Y1a)
-                this.fPoints[1] = new PointF(-50 * tX1 / tY1 + origin.X,  700f / tY1 + origin.Y); // (X1, Y1b)
-                this.fPoints[2] = new PointF(-50 * tX2 / tY2 + origin.X,  700f / tY2 + origin.Y); // (X2, Y2b)
-                this.fPoints[3] = new PointF(-50 * tX2 / tY2 + origin.X, -700f / tY2 + origin.Y); // (X2, Y2a)
+                this.fPoints[0] = new PointF(-hfov * tX1 / tY1 + origin.X, -vfov / tY1 + origin.Y); // (X1, Y1a) RED
+                this.fPoints[1] = new PointF(-hfov * tX1 / tY1 + origin.X,  vfov / tY1 + origin.Y); // (X1, Y1b) BLUE
+                this.fPoints[2] = new PointF(-hfov * tX2 / tY2 + origin.X,  vfov / tY2 + origin.Y); // (X2, Y2b) GREEN
+                this.fPoints[3] = new PointF(-hfov * tX2 / tY2 + origin.X, -vfov / tY2 + origin.Y); // (X2, Y2a) YELLOW
 
                 /*
                 this.fPoints[0] = new PointF(-16 * wall.point1.X / wall.point1.Y, -50 / wall.point1.Y); // (X1, Y1a)
@@ -83,16 +90,16 @@ namespace TransFormation
 
         public PointF intersect(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
         {
-            PointF iSx = new PointF();
+            PointF iSect = new PointF();
             float cross(float a, float b, float c, float d) { return a * d - b * c; }
 
             float ix = cross(x1, y1, x2, y2);
             float iy = cross(x3, y3, x4, y4);
             float det = cross(x1 - x2, y1 - y2, x3 - x4, y3 - y4);
-            iSx.X = cross(ix, x1 - x2, iy, x3 - x4) / det;
-            iSx.Y = cross(ix, y1 - y2, iy, y3 - y4) / det;
+            iSect.X = cross(ix, x1 - x2, iy, x3 - x4) / det;
+            iSect.Y = cross(ix, y1 - y2, iy, y3 - y4) / det;
 
-            return iSx;
+            return iSect;
         }
 
         public void drawWalls(Graphics canvas)
@@ -100,6 +107,19 @@ namespace TransFormation
             Pen wallColour = new Pen(Brushes.DarkOrange, 5);
 
             canvas.DrawPolygon(wallColour, fPoints);
+
+            RectangleF calcRect(PointF point)
+            {
+                float size = 6;
+                RectangleF rectangle = new RectangleF(point.X - size, point.Y - size, size, size);
+                return rectangle;
+            }
+
+            canvas.FillEllipse(Brushes.Red, calcRect(fPoints[0]));    // (X1, Y1a)
+            canvas.FillEllipse(Brushes.Blue, calcRect(fPoints[1]));   // (X1, Y1b)
+            canvas.FillEllipse(Brushes.Green, calcRect(fPoints[2]));  // (X2, Y2b)
+            canvas.FillEllipse(Brushes.Yellow, calcRect(fPoints[3])); // (X2, Y2a)
+
         }
 
         public void drawGradient(Graphics canvas)
